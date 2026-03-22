b67806669607a9a369ab428e06ab0ef04370f7bf
Milk-Cool <coder.tomsk@gmail.com>
2026-03-22 23:02:48 +0300

    meow

---
+++ bot.ts
@@ -0,0 +1,11 @@
+import { Telegraf } from "telegraf";
+
+const allowed = ["wiki", "vid", "pic", "gif"];
+
+const bot = new Telegraf(process.env.TOKEN!);
+bot.on("message", ctx => {
+    if("via_bot" in ctx.message && ctx.message.via_bot && !allowed.includes(ctx.message.via_bot.username!))
+        bot.telegram.deleteMessage(ctx.chat.id, ctx.message.message_id);
+});
+
+bot.launch();
