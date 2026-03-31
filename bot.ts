27ce5e8cb42e52aacd2aa69ca84a088c9948f21b
Milk-Cool <coder.tomsk@gmail.com>
2026-03-31 14:21:55 +0300

    moo

--- bot.ts
+++ bot.ts
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import { Telegraf } from "telegraf";
 
+const disallowed = process.env.DISALLOWED ? process.env.DISALLOWED.split(",").map(x => x.trim().toLowerCase()) : [];
 const allowed = process.env.ALLOWED ? process.env.ALLOWED.split(",").map(x => x.trim().toLowerCase()) : ["wiki", "vid", "pic", "gif"];
 
 const bot = new Telegraf(process.env.TOKEN!);
 bot.on("message", ctx => {
-    if("via_bot" in ctx.message && ctx.message.via_bot && !allowed.includes(ctx.message.via_bot.username!.toLowerCase()))
+    if("via_bot" in ctx.message && ctx.message.via_bot
+            && (disallowed.length === 0
+            ? !allowed.includes(ctx.message.via_bot.username!.toLowerCase())
+            : disallowed.includes(ctx.message.via_bot.username!.toLowerCase())))
         bot.telegram.deleteMessage(ctx.chat.id, ctx.message.message_id);
 });
 
